ef7b5b5ea38378aaec04a34bb0a6a84924b2cb26
LucaSilverhand <levinswap0@gmail.com>
2021-05-29 02:29:23 +0400

    farming pairs

--- src/sushi/lib/constants.js
+++ src/sushi/lib/constants.js
@@ -129,6 +129,33 @@ export const supportedPools = [
     icon: '⚡',
   },
   {
+    pid: 25,
+    lpAddresses: {
+      100: '0xd024224a5370e210e287840b68f2da544d2d9884',
+    },
+    tokenAddresses: {
+      100: '0x1698cd22278ef6e7c0df45a8dea72edbea9e42aa',
+    },
+    name: 'LEVIN/REALT 13606',
+    symbol: 'LEVIN/REALT 13606 LP',
+    tokenSymbol: 'LEVIN',
+    icon: '⚡',
+  },
+  {
+    pid: 26,
+    lpAddresses: {
+      100: '0x4d6365d0a255969cc214153fe92bc8d17b1ec614',
+    },
+    tokenAddresses: {
+      100: '0x1698cd22278ef6e7c0df45a8dea72edbea9e42aa',
+    },
+    name: 'LEVIN/REALT 12409',
+    symbol: 'LEVIN/REALT 12409 LP',
+    tokenSymbol: 'LEVIN',
+    icon: '⚡',
+  },
+  // new inactive pools
+  {
     pid: 23,
     lpAddresses: {
       100: '0x8e25e0d2b042a80507578132d45fe65f9147c882',
@@ -136,8 +163,8 @@ export const supportedPools = [
     tokenAddresses: {
       100: '0x1698cd22278ef6e7c0df45a8dea72edbea9e42aa',
     },
-    name: 'LEVIN/REALT 11078',
-    symbol: 'LEVIN-REALT 11078 LP',
+    name: 'REALT 11078 (Ended)',
+    symbol: 'REALT 11078 LP',
     tokenSymbol: 'LEVIN',
     icon: '⚡',
   },
@@ -149,8 +176,8 @@ export const supportedPools = [
     tokenAddresses: {
       100: '0x1698cd22278ef6e7c0df45a8dea72edbea9e42aa',
     },
-    name: 'LEVIN/REALT 18983',
-    symbol: 'LEVIN-REALT 18983 LP',
+    name: 'REALT 18983 (Ended)',
+    symbol: 'REALT 18983 LP',
     tokenSymbol: 'LEVIN',
     icon: '⚡',
   },
